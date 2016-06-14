fe3a49002a6cca4e8f1e36696a7f288677f03ba3
welckron <rjmagalhaesteixeira@gmail.com>
2016-06-14 14:42:03 +0100

    mudanças nos nomes

--- DBT.el
+++ DBT.el
@@ -3,8 +3,8 @@ import "Interfaces.el"
 import "SourceArchitecture.el"
 import "TargetArchitecture.el"
 import "Engine.el"
-import "SourceGang.el"
-import "TargetGang.el"
+import "SourceGroup.el"
+import "TargetGroup.el"
 import "CodeCache.el"
 import "TranslationCache.el"
 
@@ -19,29 +19,41 @@ import "DataMemory.el"
 component DBT (C)
 {
 	subcomponents:
-		SourceGang srcGang
-		TargetGang trgGang
+		SourceGroup srcGroup
+		TargetGroup trgGroup
 		CodeCache ccache
 		TranslationCache tcache
-		Engine eng
+		Engine engine
 		
 	references:
 		inter16 run
 		
-	bind run 				to eng.run
-	bind trgGang.src_reg 	to srcGang.reg
-	bind trgGang.pc 		to srcGang.pc
-	bind srcGang.fetch 		to ccache.fetch
-	bind ccache.getWordSize to srcGang.scr_word
-	bind eng.decode 		to srcGang.decode
-	bind eng.mem 			to srcGang.mem
-	bind eng.fetch 			to ccache.fetch
-	bind eng.generate 		to trgGang.generate
-	bind eng.get_ptr 		to tcache.getLastTransAddr
-	bind eng.set_ptr 		to tcache.getCurrInsAddr
-	bind trgGang.cache_code	to tcache.cacheCode
-	bind tcache.getWordSize to trgGang.target_word
-	bind eng.addTag			to tcache.addTag
-	bind eng.getTransAddr	to tcache.getTransAddr
+	bind run 					to engine.run
+	bind engine.generate 		to trgGroup.generate
+	
+	// Bindings to the SourceGang Services
+	bind trgGroup.src_reg 		to srcGroup.reg
+	bind trgGroup.pc 			to srcGroup.pc
+	bind engine.decode 			to srcGroup.decode
+	bind engine.mem 			to srcGroup.getDataMem
+	
+	
+	// DONE 
+	// Bindings to the CCache References
+	bind ccache.getWordSize 	to srcGroup.setWordSize
+	
+	// Bindings to the CCache Services
+	bind engine.fetch 			to ccache.fetch
+	bind srcGroup.fetch 		to ccache.fetch
+	
+	// Bindings to the TCache References
+	bind tcache.getWordSize 	to trgGroup.setWordSize
+	
+	// Bindings to the TCache Services 
+	bind engine.get_ptr 		to tcache.getLastTransAddr 
+	bind engine.set_ptr 		to tcache.getCurrInsAddr
+	bind engine.addTag			to tcache.addTag
+	bind engine.getTransAddr	to tcache.getTransAddr
+	bind trgGroup.cacheCode		to tcache.cacheCode
 }
 
---
+++ SourceGroup.el
@@ -0,0 +1,27 @@
+import "Languages.el"
+import "Interfaces.el"
+import "Decoder.el"
+import "SourceArchitecture.el"
+import "SourceEnvironment.el"
+
+component SourceGroup (C)
+{
+	subcomponents:
+		Decoder decode
+		SourceArch srcArch
+		SourceEnv srcEnv	
+		
+	bind srcEnv.getMemSize 	to srcArch.memSize
+	bind srcEnv.getXMemSize to srcArch.xMemSize
+	bind decode.pc 			to srcEnv.pc
+	bind decode.op 			to srcArch.op
+	bind decode.reg 		to srcArch.reg
+	bind decode.getWordSize to srcArch.setWordSize
+	
+	promote service srcArch.reg 		as reg
+	promote service srcArch.setWordSize as setWordSize
+	promote service srcEnv.getDataMem 	as getDataMem  // Atualizado - Não é necessário fazer promote (Ver SourceEnv)
+	promote service srcEnv.s_pc 		as pc
+	promote service decode.decode 		as decode
+	promote reference decode.fetch 		as fetch
+}
